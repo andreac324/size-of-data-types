b541f5f1ce51be61c1324ccb38fdb998e9497c61
andreac324 <terisa32873@gmail.com>
2024-09-14 22:12:30 -0700

    Initial Commit

---
+++ ca5/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ca5
+//
+//  Created by Andrea d Cazares on 9/14/24.
+//
+
+#include <iostream>
+using namespace std;
+int main(){
+    short shortVariable;
+    double doubleVariable;
+
+    cout<< "size of short is: "<< sizeof(shortVariable)<<endl;
+
+     cout<< "size of double is: "<< sizeof(doubleVariable)<<endl;
+    return 0;
+}
